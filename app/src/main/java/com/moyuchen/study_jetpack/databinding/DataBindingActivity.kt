db3dbbc708adb7a7b4e16ac495df9ffb15b80af6
moyuchen <moyuchenfirst@126.com>
2021-06-03 16:24:01 +0800

    DataBinding绑定视图，在activity中直接绑定layout布局

---
+++ app/src/main/java/com/moyuchen/study_jetpack/databinding/DataBindingActivity.kt
@@ -0,0 +1,22 @@
+package com.moyuchen.study_jetpack.databinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.moyuchen.study_jetpack.R
+
+class DataBindingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val dataBinding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_data_binding
+        ) as ActivityDataBindingBinding
+        var entity = DataBindingEntity()
+        entity.name = "张三"
+        entity.position = "北京"
+        dataBinding.dataBindingEntity = entity
+
+
+    }
+}
